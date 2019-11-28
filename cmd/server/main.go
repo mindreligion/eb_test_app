62fef153f60f0bb1e96e2729705f9861c616dc04
adelibazohlo <andrii.delibazohlo@dgngames.com>
2019-11-28 18:53:58 +0200

    fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ func main() {
 }
 
 func rootH(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello , %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello  , %s!", r.URL.Path[1:])
 	logger.WithFields(logrus.Fields{"urlPath": r.URL.Path[1:]}).Error("invalid actions happened")
 
 }
